f8893c8082173d9d64fee36e88f51821d72ec33a
CodeWizard812 <shirish.professional@gmail.com>
2025-04-14 15:55:49 +0530

    Basic templating

--- food/views.py
+++ food/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import loader
 
 
 # Create your views here.
 def index(request):
     items_list = Item.objects.all()
-    return HttpResponse(items_list)
+    context = {
+        'items_list': items_list,
+    }
+    return render(request, 'food/index.html', context)
 
 def item(request):
     return HttpResponse("Hello, world. You're at the food item.")
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item,
+    }
+    return render(request, 'food/detail.html', context)
