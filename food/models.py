3ac95523d41d7b93aeb413381273e2834ca93655
CodeWizard812 <shirish.professional@gmail.com>
2025-04-14 18:20:21 +0530

    Static files created, image added to model

--- food/models.py
+++ food/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_description = models.TextField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://cleanplatesdetroit.com/data/meals/1289.jpg")
 
     def __str__(self):
         return self.item_name
