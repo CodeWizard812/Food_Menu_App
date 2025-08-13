4b00d32e8edefb8087fbaea85954663885eff191
CodeWizard812 <shirish.professional@gmail.com>
2025-08-13 20:24:08 +0530

    Host added

--- Food_Menu_App/settings.py
+++ Food_Menu_App/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codewizard812.pythonanywhere.com']
 
 
 # Application definition
